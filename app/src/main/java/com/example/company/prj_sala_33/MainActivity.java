10f23376af9f46d17dfc70d01ac151f19a94a6b6
Thassiano <Thassianosoares@hotmail.com>
2018-02-10 21:57:08 -0200

    Atualização de Pagina Principal

--- app/src/main/java/com/example/company/prj_sala_33/MainActivity.java
+++ app/src/main/java/com/example/company/prj_sala_33/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.company.prj_sala_33;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.telalogin);
+        setContentView(R.layout.telaprincipal);
     }
+
 }
