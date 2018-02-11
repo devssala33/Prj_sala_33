3fc48ce42dc1b80514df8214ebc071fedb982b4d
MilRubro <miltonrubro@gmail.com>
2018-02-11 17:21:11 -0200

    finalizando logica da tela de login

---
+++ app/src/main/java/com/example/company/prj_sala_33/views/TelaPrincipal.java
@@ -0,0 +1,23 @@
+package com.example.company.prj_sala_33.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.company.prj_sala_33.R;
+
+/**
+ * Created by milton on 11/02/2018.
+ */
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.telaprincipal);
+
+
+
+    }
+}
